f282190b57803ec7f4111d23c9e9dc6f4ad874c9
cmiller12312 <calemiller76@gmail.com>
2025-06-10 16:09:48 -0500

    started working on backendHandlers actual functionality. Can now send direct login and signup requests to server and tokens are auto stored in the connection instance to act as the session. Will make token a private variable soon.

--- backendHandler.py
+++ backendHandler.py
@@ -1,15 +1,36 @@
 import requests
+import json
+import os
 
 class connection:
     def __init__(self, host, *args, **kwargs):
         self.host = host
+        self.token = None
 
     def post(self, data):
         request = requests.post(url=self.host, json=data)
-        print(request.reason)
+        print(request.json())
+        print(request.json()["message"])
+
+    def login(self, data):
+        try:
+            request = requests.post(self.host + "/login/", json=data)
+            self.token = request.json()["token"]
+            return True, None
+        except:
+            return False, request.json()["message"]
         
+    def signUp(self, data):
+        try:
+            request = requests.post(self.host + "/signup/", json=data)
+            self.token = request.json()["token"]
+            return True, None
+        except:
+            return False, request.json()["message"]
+
 
 
 if __name__ == "__main__":
-    data = connection("http://127.0.0.1:8000/signup/").post({"username":"user2", "password":"password"})
+    print(connection("http://127.0.0.1:8000/").signUp({"username":"user2", "password":"password"}))
+    print(connection("http://127.0.0.1:8000/").login({"username":"user2", "password":"password"}))
     
