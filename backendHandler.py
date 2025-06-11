91c9ab7d4e421f3c8be7c5b25ff09e2129ec3c01
cmiller12312 <calemiller76@gmail.com>
2025-06-11 15:08:20 -0500

    started working on upload video api endpoint. Client side will change a lot

--- backendHandler.py
+++ backendHandler.py
@@ -15,22 +15,41 @@ class connection:
     def login(self, data):
         try:
             request = requests.post(self.host + "/login/", json=data)
-            self.token = request.json()["token"]
+            self.token = "Token " + request.json()["token"]
             return True, None
         except:
-            return False, request.json()["message"]
+            try:
+                return False, request.json()["message"]
+            except:
+                return False, "Failed to connect"
         
     def signUp(self, data):
         try:
             request = requests.post(self.host + "/signup/", json=data)
-            self.token = request.json()["token"]
+            self.token = "Token " + request.json()["token"]
             return True, None
         except:
-            return False, request.json()["message"]
+            try:
+                return False, request.json()["message"]
+            except:
+                return False, "Failed to connect"
+            
+    def uploadVideo(self, data):
+        try:
+            request = requests.post(self.host + "/uploadVideo/", json=data, headers={"Authorization": self.token})
+            return True, None
+        except:
+            try:
+                return False, request.json()["message"]
+            except:
+                return False, "Failed to connect"
 
 
 
 if __name__ == "__main__":
+
+    #print(connection("http://127.0.0.1:8000/uploadVideo/").post({"data":"data"}))
     print(connection("http://127.0.0.1:8000/").signUp({"username":"user2", "password":"password"}))
-    print(connection("http://127.0.0.1:8000/").login({"username":"user2", "password":"password"}))
-    
+    temp = connection("http://127.0.0.1:8000/")
+    temp.login({"username":"user2", "password":"password"})
+    print(temp.uploadVideo({"message":"hi"}))
