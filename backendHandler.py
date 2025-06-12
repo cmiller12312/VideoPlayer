f598ca33044fa46da28b5c3011da21a00d8498ba
cmiller12312 <calemiller76@gmail.com>
2025-06-12 10:35:59 -0500

    worked on uploadVideo

--- backendHandler.py
+++ backendHandler.py
@@ -36,13 +36,20 @@ class connection:
             
     def uploadVideo(self, data):
         try:
-            request = requests.post(self.host + "/uploadVideo/", json=data, headers={"Authorization": self.token})
-            return True, None
+            path = data["videoPath"]
+        except:
+            return False, "video path is not specified"
+        try:
+            with open(path, "rb") as file:
+                request = requests.post(self.host + "/uploadVideo/", data=data, headers={"Authorization": self.token}, files={"video": file})
         except:
             try:
                 return False, request.json()["message"]
             except:
                 return False, "Failed to connect"
+        return True, None
+
+            
 
 
 
@@ -52,4 +59,7 @@ if __name__ == "__main__":
     print(connection("http://127.0.0.1:8000/").signUp({"username":"user2", "password":"password"}))
     temp = connection("http://127.0.0.1:8000/")
     temp.login({"username":"user2", "password":"password"})
-    print(temp.uploadVideo({"message":"hi"}))
+    current_dir = os.path.dirname(__file__)
+    video_path = os.path.join(current_dir, "video.mp4")
+    tags = json.dumps(["testTag", "testTag2"])
+    print(temp.uploadVideo({"title":"superCoolVideo","description":"my cool new video", "videoPath": video_path, "tags": tags }))
--- fileSender.py
+++
@@ -1,9 +0,0 @@
-import ftplib
-
-class mp4Sender():
-    with ftplib("https://docs.python.org/3/library/ftplib.html") as ftp:
-        ftp.connect(host="localhost", port = 21)
-
-
-
-    
